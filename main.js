ddbb5f02c0f259cc5e23e6e953cd26bfd12655cd
Nea18 <anne.champenaud@yahoo.fr>
2017-10-05 14:57:02 +0200

    Fonction affichage éléments centraux

---
+++ main.js
@@ -0,0 +1,16 @@
+let apparition = document.getElementById("centraux");
+let button = document.getElementById("display");
+
+button.onclick = function ()
+{
+apparition.style.display = 'block';
+button.style.display = 'none';
+}
+
+
+/*document.getElementById('centraux').style.display = 'none';
+
+function display() {
+    document.getElementById('centraux'.style.display = 'block';
+    document.getElementById('display').style.display = 'none';
+}*/
